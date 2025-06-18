d1640017c400537b4fc81a7a0de0c25b25f4375d
ishajivani98 <ishajivani88@gmail.com>
2025-06-18 15:11:36 +0530

    swap number without using three variable

---
+++ array/swapNum.js
@@ -0,0 +1,15 @@
+//  Swap Two Variables Without a Third Variable
+// Write a program to swap values of two variables without using a third variable.
+
+
+let a=10
+let b=20
+
+console.log('a value after swaping ', a)
+console.log('b value after swaping ',b)
+
+a=a+b
+b=a-b
+a=a-b
+console.log('now  value of a is :',a)
+console.log('now  value of b is :',b)
