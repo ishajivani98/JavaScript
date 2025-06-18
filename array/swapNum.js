6160cbc04e1fc4ad844e4ca53c0a79e658168797
ishajivani98 <ishajivani88@gmail.com>
2025-06-18 15:16:54 +0530

    swap number without using three variable

--- array/swapNum.js
+++ array/swapNum.js
@@ -2,14 +2,33 @@
 // Write a program to swap values of two variables without using a third variable.
 
 
-let a=10
-let b=20
+// let a=10
+// let b=20
 
-console.log('a value after swaping ', a)
-console.log('b value after swaping ',b)
+// console.log('a value before swaping ', a)
+// console.log('b value before swaping ',b)
 
-a=a+b
-b=a-b
-a=a-b
-console.log('now  value of a is :',a)
-console.log('now  value of b is :',b)
+// a=a+b
+// b=a-b
+// a=a-b
+
+// console.log('now  value of a is :',a)
+// console.log('now  value of b is :',b
+
+
+// Using Array Destructuring 
+
+
+let a = 10;
+let b = 20;
+
+console.log("Before Swapping:");
+console.log("a =", a);
+console.log("b =", b);
+
+// Swap using destructuring
+[a, b] = [b, a];
+
+console.log("After Swapping:");
+console.log("a =", a);
+console.log("b =", b);
